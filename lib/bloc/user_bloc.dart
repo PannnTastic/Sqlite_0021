43eb9ff3426ea09f9703edb165ab8e3b16045b52
PannnTastic <pannntastic@gmail.com>
2026-04-24 10:43:01 +0700

    feat: implement UserBloc to handle CRUD operations for user management

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -1,13 +1,55 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'user_event.dart';
-part 'user_state.dart';
+import 'package:pertemuan8/bloc/user_event.dart';
+import 'package:pertemuan8/bloc/user_state.dart';
+import 'package:pertemuan8/domain/repository/user_repository.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc() : super(UserInitial()) {
-    on<UserEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final UserRepository userRepository;
+
+  UserBloc(this.userRepository) : super(UserInitial()){
+    on<LoadUsers>(_onLoadUsers);
+    on<AddUser>(_onAddUser);
+    on<UpdateUser>(_onUpdateUser);
+    on<DeleteUser>(_onDeleteUser);
+  }
+
+  Future<void> _onLoadUsers(LoadUsers event, Emitter<UserState> emit) async{
+    emit(UserLoading());
+    try{
+      final users = await userRepository.getAllUsers();
+      emit(UserLoaded(users));
+    }catch(e){
+      emit(UserError(e.toString()));
+    }
+  }
+
+  Future<void> _onAddUser(AddUser event, Emitter<UserState> emit) async{
+    try{
+      await userRepository.addUser(event.user);
+      final users = await userRepository.getAllUsers();
+      emit(UserLoaded(users));
+    }catch(e){
+      emit(UserError(e.toString()));
+    }
+  }
+
+  Future<void> _onUpdateUser(UpdateUser event, Emitter<UserState> emit) async{
+    try{
+      await userRepository.updateUser(event.user);
+      final users = await userRepository.getAllUsers();
+      emit(UserLoaded(users));
+    }catch(e){
+      emit(UserError(e.toString()));
+    }
+  }
+
+  Future<void> _onDeleteUser(DeleteUser event, Emitter<UserState> emit) async{
+    try{
+      await userRepository.deleteUser(event.id);
+      final users = await userRepository.getAllUsers();
+      emit(UserLoaded(users));
+    }catch(e){
+      emit(UserError(e.toString()));
+    }
   }
 }
