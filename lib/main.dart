844984424acea6af1000e64dd6d5a2486adc268d
PannnTastic <pannntastic@gmail.com>
2026-04-24 15:40:40 +0700

    feat: implement user form page with phone number validation using flutter_libphonenumber

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_libphonenumber/flutter_libphonenumber.dart';
 import 'package:pertemuan8/bloc/user_bloc.dart';
 import 'package:pertemuan8/bloc/user_event.dart';
 import 'package:pertemuan8/data/repositories/user_repository_impl.dart';
 import 'package:pertemuan8/helper/database_helper.dart';
 import 'package:pertemuan8/pages/user_list_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init(); // Inisialisasi flutter_libphonenumber
   runApp(const MyApp());
 }
 
