a19973c0d6b61aba331f8af06bf5401e7da4a0e6
PannnTastic <pannntastic@gmail.com>
2026-04-24 11:06:23 +0700

    feat: add sqflite_darwin plugin and remove unused flutter_libphonenumber dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_libphonenumber_darwin
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterLibphonenumberPlugin.register(with: registry.registrar(forPlugin: "FlutterLibphonenumberPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
